28b852f49d644632b59f346e8451637ad356dfdc
hahava <hahava5696@gmail.com>
2020-06-16 21:35:06 +0900

    Add request body validation

--- src/main/java/capstone/sejong/inquiry/InquiryController.java
+++ src/main/java/capstone/sejong/inquiry/InquiryController.java
@@ -3,21 +3,31 @@ package capstone.sejong.inquiry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class InquiryController {
 
 	private static final int INSERT_SUCCESS = 1;
+	private static final int FIRST_ERROR = 0;
 
 	@Autowired
 	private InquiryService service;
 
 	@RequestMapping(value = "/inquiry", method = RequestMethod.POST)
-	public ResponseEntity addInquiry(@RequestBody InquiryDTO inquiryDTO) {
+	public ResponseEntity addInquiry(@Valid @RequestBody InquiryDTO inquiryDTO, BindingResult br) {
+
+		if (br.hasErrors()) {
+			String errMsg = br.getAllErrors().get(FIRST_ERROR).getDefaultMessage();
+			return new ResponseEntity("Error : " + errMsg, HttpStatus.BAD_REQUEST);
+		}
+
 		int result = service.addInquiry(inquiryDTO);
 		if (result == INSERT_SUCCESS) {
 			return new ResponseEntity("success", HttpStatus.OK);
--- src/main/java/capstone/sejong/inquiry/InquiryDTO.java
+++ src/main/java/capstone/sejong/inquiry/InquiryDTO.java
@@ -4,12 +4,24 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @ToString
 public class InquiryDTO {
+	@NotNull(message = "Email must be included")
 	private String email;
+
+	@NotNull(message = "Title must be included")
+	@Size(max = 100, message = "Title must be under 100 chars")
 	private String title;
+
+	@NotNull
+	@Size(max = 500, message = "Content must be under 100 chars")
 	private String content;
+
+	@NotNull(message = "Type must be included")
 	private String type;
 }
